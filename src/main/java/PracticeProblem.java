725945828484c56d355ce24a7eaf2f9071fd51d8
ericgranitewang <166552552+ericgranitewang@users.noreply.github.com>
2026-03-30 13:47:03 +0000

    commit real

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,21 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int sumOfDigits(int x) {
+		x = Math.abs(x);
+		if (x == 0) {
+			return 0;
+		}
+		return x%10 + sumOfDigits(x/10);
+	}
 
+	public static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		}
+		if (n < 0) {
+			return 0;
+		}
+		return n*factorial(n-1);
+	}
 }
